4b157fda821e0e34f65f7947346b279363145ef8
moral <moraldianneangelpios@gmail.com>
2022-10-19 11:31:43 +0800

    Project

---
+++ app/src/main/java/com/example/homeworktracker/Addcourse.java
@@ -0,0 +1,14 @@
+package com.example.homeworktracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Addcourse extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addcourse);
+    }
+}
