2951e171232b10c489af57ef70fbba90c98905e2
moral <moraldianneangelpios@gmail.com>
2022-11-16 17:01:42 +0800

    Project

--- app/src/main/java/com/example/homeworktracker/MainActivity.java
+++ app/src/main/java/com/example/homeworktracker/MainActivity.java
@@ -1,21 +1,27 @@
 package com.example.homeworktracker;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+ private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button button =findViewById(R.id.button2);
-        button.setOnClickListener(new View.OnClickListener() {
+        drawerLayout =findViewById(R.id.drawer_layout);
+        Button button=findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, addassignment.class);
@@ -34,7 +40,62 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+    public void ClickMenu(View view){
+        openDrawer(drawerLayout);
+    }
+    public static void openDrawer(DrawerLayout drawerLayout){
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+    public void ClickLogo(View view){
+        closeDrawer(drawerLayout);
+    }
+    public static void closeDrawer(DrawerLayout drawerLayout){
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+    }
+    public void ClickHome(View view){
+        recreate();
+    }
+    public void ClickCourse(View view){
+        redirectActivity(this,Courses.class );
+    }
 
+    public void ClickLogout(View view){
+        logout(this);
+    }
+    public static void logout (MainActivity activity) {
+        AlertDialog.Builder builder=new AlertDialog.Builder(activity);
+        builder.setTitle("Logout");
+        builder.setMessage("Are you sure you want to log out?");
+        builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                activity.finishAffinity();
+                System.exit(0);
+            }
+        });
+        builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.show();
+    }
+
+    public static void redirectActivity(MainActivity activity, Class aClass){
+        Intent intent=new Intent (activity, aClass);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        activity.startActivity(intent);
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        closeDrawer(drawerLayout);
     }
 
 
+
+
+}
